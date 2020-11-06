a2945745363dd5bd9e7612bf3f2e0da3fa14a314
internalinterference <kanequbolt@gmail.com>
2020-11-06 23:06:33 +1000

    Add project files.

---
+++ SimpleTodo/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //get to-do items from database 
+
+            //put items into model
+
+            //render view using model
+        }
+    }
+}
