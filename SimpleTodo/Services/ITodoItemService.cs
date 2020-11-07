cd7420f075b9df54811456f1a600c9105b9d8820
internalinterference <kanequbolt@gmail.com>
2020-11-07 18:23:31 +1000

    add some services

---
+++ SimpleTodo/Services/ITodoItemService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTodo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
